4320d114cb7da26d80487eefd6c977a6a7ce9c78
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2014-01-21 22:53:35 +0200

    First commit with the acceptance tests placed in html

---
+++ frequentwords/src/test/specs/ro/ghionoiu/frequentwords/AppFixture.java
@@ -0,0 +1,12 @@
+package ro.ghionoiu.frequentwords;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class AppFixture {
+
+    public String getFrequentWords(String firstLine, String secondLine) {
+        return "Hello ";
+    }
+}
